4d86b35e0a2db698818b685265bb5127f3b09c35
zhiyuan0312 <zhiyuan0312@hotmail.com>
2015-11-05 00:32:52 +0800

    completed login interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  root 'static#index'
+
+  get 'signup' => 'users#new'
+  resources :users
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  resources :sessions
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
