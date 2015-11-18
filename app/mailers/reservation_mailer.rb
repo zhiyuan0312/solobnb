fa65d0c91bd073322700dfbd3e9266f9b9a23f51
zhiyuan0312 <zhiyuan0312@hotmail.com>
2015-11-18 17:22:19 +0800

    completed action mailer

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,7 +1,11 @@
 class ReservationMailer < ApplicationMailer
-	def booking_email(customer, host, reservation_id)
+	def booking_email(customer, host, reservation)
 		@host = host
 		@customer = customer
-		mail(to: @host, subject: "You have received a booking form #{@custome}")
+		@reservation = reservation
+
+		mail(to: "zhiyuan0312@gmail.com", subject: "You have received a booking from #{@customer.first_name} #{@customer.last_name}")
 	end
+
+
 end
--- test/mailers/previews/reservation_mailer_preview.rb
+++ test/mailers/previews/reservation_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/reservation_mailer
 class ReservationMailerPreview < ActionMailer::Preview
-
+	def booking_email_preview
+		ReservationMailer.booking_email(User.first, User.last, Listing.last.list_name)
+	end
 end
